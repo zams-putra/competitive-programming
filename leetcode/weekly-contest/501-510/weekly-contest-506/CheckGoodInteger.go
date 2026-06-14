83b73a15e0d25be50fb19fb993853b8337dbee65
zams-putra <penambahsubreker57@gmail.com>
2026-06-14 10:34:13 +0700

    add: new

---
+++ leetcode/weekly-contest/501-510/weekly-contest-506/CheckGoodInteger.go
@@ -0,0 +1,43 @@
+package weeklycontest506
+
+import "strconv"
+
+// Question :
+
+// You are given a positive integer n.
+// Let digitSum be the sum of the digits of n, and let squareSum be the sum of the squares of the digits of n.
+// An integer is called good if squareSum - digitSum >= 50.
+// Return true if n is good. Otherwise, return false.
+//
+// Example 1:
+// Input: n = 1000
+// Output: false
+// Explanation:
+// The digits of 1000 are 1, 0, 0, and 0.
+// The digitSum is 1 + 0 + 0 + 0 = 1.
+// The squareSum is 12 + 02 + 02 + 02 = 1.
+// The squareSum - digitSum is 1 - 1 = 0. As 0 is not greater than or equal to 50, the output is false.
+// Example 2:
+// Input: n = 19
+// Output: true
+// Explanation:
+// The digits of 19 are 1 and 9.
+// The digitSum is 1 + 9 = 10.
+// The squareSum is 12 + 92 = 1 + 81 = 82.
+// The squareSum - digitSum is 82 - 10 = 72. As 72 is greater than or equal to 50, the output is true.
+//
+// Constraints:
+// 1 <= n <= 109
+
+// Answer :
+
+func CheckGoodInteger(n int) bool {
+	digitSum, squareSum := 0, 0
+	s := strconv.Itoa(n)
+	for i := 0; i < len(s); i++ {
+		nI, _ := strconv.Atoi(string(s[i]))
+		digitSum += nI
+		squareSum += nI * nI
+	}
+	return (squareSum - digitSum) >= 50
+}
